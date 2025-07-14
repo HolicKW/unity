2f11c884796342228d1633288eb87c37081bcabe
HolicKW <gkaeodud12@gmail.com>
2025-07-14 19:40:23 +0900

    Update
    Update

---
+++ MRun/Assets/Scripts/ObjectMaker.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMaker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject[] floorObject;
+    [SerializeField]
+    private GameObject[] snailObject;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
